d5b6ad2225000493a49790a34569f1e19d1dcab5
gsikharulidze <mwvanegren@gmail.com>
2017-11-23 16:58:57 +0400

    add collor guide for help commands

--- TodoInFileStorage/CommandProcessors/HelpCommandProcessor.cs
+++ TodoInFileStorage/CommandProcessors/HelpCommandProcessor.cs
@@ -6,12 +6,18 @@ namespace TodoInFileStorage
     {
         public override void Process()
         {
+            Console.WriteLine("Collor guide:");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow:\t- Command");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red:\t- Error ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green:\t- Result (After success command)");
             Console.WriteLine();
             Console.WriteLine("avaliable commands");
-            Console.WriteLine("==============================");
+            Console.WriteLine("------------------------------");
             Tasks.Help();
-            Console.WriteLine();
 
         }
     }
--- TodoInFileStorage/Program.cs
+++ TodoInFileStorage/Program.cs
@@ -55,7 +55,7 @@ namespace TodoInFileStorage
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine();
-                Console.WriteLine("command not recognized, please use 'help' command and see avaliable commands");
+                Console.WriteLine("command not recognized, please use 'help' command and see all avaliable commands");
             }
         }
     }
